8e2eab4352b1c092ffe35a35a995938046013c00
lucko07 <freedomfinancial70@gmail.com>
2026-03-16 21:06:52 -0400

    Wrap routes with BrowserRouter

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import PricingPage from "./pages/PricingPage";
@@ -19,61 +19,63 @@ import UpgradeCancelPage from "./pages/UpgradeCancelPage";
 
 function App() {
   return (
-    <Routes>
-      <Route element={<MainLayout />}>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/pricing" element={<PricingPage />} />
-        <Route path="/how-it-works" element={<HowItWorksPage />} />
-        <Route path="/signals" element={<SignalsPage />} />
-        <Route path="/education" element={<EducationPage />} />
-        <Route path="/faq" element={<FaqPage />} />
-        <Route path="/contact" element={<ContactPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route
-          path="/upgrade"
-          element={
-            <ProtectedRoute>
-              <UpgradePage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/upgrade/success"
-          element={
-            <ProtectedRoute>
-              <UpgradeSuccessPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/upgrade/cancel"
-          element={
-            <ProtectedRoute>
-              <UpgradeCancelPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute requirePaidPlan>
-              <DashboardPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route
-          path="/admin/signals"
-          element={
-            <ProtectedRoute requireAdmin>
-              <AdminSignalsPage />
-            </ProtectedRoute>
-          }
-        />
-        <Route path="/home" element={<Navigate to="/" replace />} />
-        <Route path="*" element={<NotFoundPage />} />
-      </Route>
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<MainLayout />}>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/pricing" element={<PricingPage />} />
+          <Route path="/how-it-works" element={<HowItWorksPage />} />
+          <Route path="/signals" element={<SignalsPage />} />
+          <Route path="/education" element={<EducationPage />} />
+          <Route path="/faq" element={<FaqPage />} />
+          <Route path="/contact" element={<ContactPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignupPage />} />
+          <Route
+            path="/upgrade"
+            element={
+              <ProtectedRoute>
+                <UpgradePage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/upgrade/success"
+            element={
+              <ProtectedRoute>
+                <UpgradeSuccessPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/upgrade/cancel"
+            element={
+              <ProtectedRoute>
+                <UpgradeCancelPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute requirePaidPlan>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route
+            path="/admin/signals"
+            element={
+              <ProtectedRoute requireAdmin>
+                <AdminSignalsPage />
+              </ProtectedRoute>
+            }
+          />
+          <Route path="/home" element={<Navigate to="/" replace />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
